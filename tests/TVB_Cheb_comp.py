b1ab4b354e158c15bad07fbf2fc66f4dd4f55fc3
Rootfinding Group <yroots@byu.edu>
2020-12-21 12:34:38 -0700

    Bug fixes for the testing scripts.

--- tests/TVB_Cheb_comp.py
+++ tests/TVB_Cheb_comp.py
@@ -3,14 +3,15 @@ import numpy as np
 from time import time
 from random_tests import load_tests
 import pickle
+from eigen_rootfinding.utils import ConditioningError
 
 dir = 'tests/random_tests'
 
 if __name__ == "__main__":
 
     dim_degs = {2: [2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30],
-                3: [2, 3, 4, 5, 6, 7, 8, 9, 10],
-                4: [2, 3, 4, 5, 6, 7, 8],
+                3: [2, 3, 4, 5, 6, 7, 8],
+                4: [2, 3, 4, 5, 6],
                 5: [2, 3, 4, 5]}
     dims = [2, 3, 4, 5]
 
@@ -23,19 +24,35 @@ if __name__ == "__main__":
         for deg in degs:
             tests = load_tests(dim, deg, "Chebyshev", "randn")
             for method in ['svd', 'qrt', 'tvb']:
+                print(f'Starting dimension {dim} degree {deg} polynomials with {method}')
                 start = time()
                 residuals = []
-                for system in tests:
-                    # Want Residuals, condition numbers, and time
-                    roots, cond = eig_rf.solve(system)
-                    f, g = system
-                    residuals.append(f(roots))
-                    residuals.append(g(roots))
+                try:
+                    for system in tests:
+                        # Want Residuals, condition numbers, and time
+                        roots, cond = eig_rf.solve(system, method=method)
+                        for func in system:
+                            residuals.append(func(roots))
+                        end = time() - start
 
-                end = time() - start
+                        dicts[method][(dim, deg)] = {'timings': end/len(tests),
+                                                     'residuals': residuals,
+                                                     'condeigs': cond}
 
-                dicts[method][(dim, deg)] = {'timings': end/len(tests),
-                                             'residuals': residuals,
-                                             'condeigs': cond}
+                except ConditioningError as e:
+                    print(f"A Conditioning Exception occured for degree {deg} dimension {dim} polys for the {method} method.")
+                    end = time() - start
+
+                    dicts[method][(dim, deg)] = {'timings': np.nan,
+                                                 'residuals': np.nan,
+                                                 'condeigs': np.nan}
+                except Exception as e:
+                    print(f"An Exception occured for degree {deg} dimension {dim} polys for the {method} method.")
+                    end = time() - start
+
+                    dicts[method][(dim, deg)] = {'timings': np.nan,
+                                                 'residuals': np.nan,
+                                                 'condeigs': np.nan}
+
                 with open(f'{dir}/{method}_results.pkl', 'wb') as ofile:
                     pickle.dump(dicts[method], ofile)
