3d117b8eacc2fb19b98b912050ecbdc84263648a
Suzann <suzannastep@gmail.com>
2021-01-07 21:30:56 -0600

    prettier printing of process ids

--- tests/rand_dev_conditioning_ratios.sh
+++ tests/rand_dev_conditioning_ratios.sh
@@ -10,5 +10,5 @@ do
   nohup python3 -u conditioning_ratios.py rand $newton $randN $dim > $randfolder"$dim"/out.txt 2> $randfolder"$dim"/err.txt &
   echo rand $dim $! >> rand_dev_cond_ratios_pids.txt
   nohup python3 -u conditioning_ratios.py dev  $newton $devN  $dim > $devfolder"$dim"/out.txt  2> $devfolder"$dim"/err.txt &
-  echo dev $dim $! >> rand_dev_cond_ratios_pids.txt
+  echo deva $dim $! >> rand_dev_cond_ratios_pids.txt
 done
