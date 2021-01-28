8435129aa1d7ebe9b949822fc6f3acabda3c1311
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-01-27 17:23:17 -0700

    Redirect output in test script.

--- tests/test_methods_polys.sh
+++ tests/test_methods_polys.sh
@@ -1,96 +1,95 @@
 #!/usr/bin/bash
 
 
-# Dimension 2 , comment in if we want it!
-# nohup ipython tests/test_all_methods.py 2 2 &
+# nohup ipython tests/test_all_methods.py 2 2 &>method_test_dim2_deg2 &
 # echo Dimension 2 Degree 2 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 4 &
+# nohup ipython tests/test_all_methods.py 2 4 &>method_test_dim2_deg4 &
 # echo Dimension 2 Degree 4 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 6 &
+# nohup ipython tests/test_all_methods.py 2 6 &>method_test_dim2_deg6 &
 # echo Dimension 2 Degree 6 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 8 &
+# nohup ipython tests/test_all_methods.py 2 8 &>method_test_dim2_deg8 &
 # echo Dimension 2 Degree 8 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 10 &
+# nohup ipython tests/test_all_methods.py 2 10 &>method_test_dim2_deg10 &
 # echo Dimension 2 Degree 10 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 12 &
+# nohup ipython tests/test_all_methods.py 2 12 &>method_test_dim2_deg12 &
 # echo Dimension 2 Degree 12 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 14 &
+# nohup ipython tests/test_all_methods.py 2 14 &>method_test_dim2_deg14 &
 # echo Dimension 2 Degree 14 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 16 &
+# nohup ipython tests/test_all_methods.py 2 16 &>method_test_dim2_deg16 &
 # echo Dimension 2 Degree 16 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 18 &
+# nohup ipython tests/test_all_methods.py 2 18 &>method_test_dim2_deg18 &
 # echo Dimension 2 Degree 18 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 20 &
+# nohup ipython tests/test_all_methods.py 2 20 &>method_test_dim2_deg20 &
 # echo Dimension 2 Degree 20 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 22 &
+# nohup ipython tests/test_all_methods.py 2 22 &>method_test_dim2_deg22 &
 # echo Dimension 2 Degree 22 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 24 &
+# nohup ipython tests/test_all_methods.py 2 24 &>method_test_dim2_deg24 &
 # echo Dimension 2 Degree 24 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 26 &
+# nohup ipython tests/test_all_methods.py 2 26 &>method_test_dim2_deg26 &
 # echo Dimension 2 Degree 26 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 28 &
+# nohup ipython tests/test_all_methods.py 2 28 &>method_test_dim2_deg28 &
 # echo Dimension 2 Degree 28 $! >> method_test_pids.txt
 
-# nohup ipython tests/test_all_methods.py 2 30 &
+# nohup ipython tests/test_all_methods.py 2 30 &>method_test_dim2_deg30 &
 # echo Dimension 2 Degree 30 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 2 &
+nohup ipython tests/test_all_methods.py 3 2 &>method_test_dim3_deg2 &
 echo Dimension 3 Degree 2 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 3 &
+nohup ipython tests/test_all_methods.py 3 3 &>method_test_dim3_deg3 &
 echo Dimension 3 Degree 3 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 4 &
+nohup ipython tests/test_all_methods.py 3 4 &>method_test_dim3_deg4 &
 echo Dimension 3 Degree 4 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 5 &
+nohup ipython tests/test_all_methods.py 3 5 &>method_test_dim3_deg5 &
 echo Dimension 3 Degree 5 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 6 &
+nohup ipython tests/test_all_methods.py 3 6 &>method_test_dim3_deg6 &
 echo Dimension 3 Degree 6 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 7 &
+nohup ipython tests/test_all_methods.py 3 7 &>method_test_dim3_deg7 &
 echo Dimension 3 Degree 7 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 8 &
+nohup ipython tests/test_all_methods.py 3 8 &>method_test_dim3_deg8 &
 echo Dimension 3 Degree 8 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 9 &
+nohup ipython tests/test_all_methods.py 3 9 &>method_test_dim3_deg9 &
 echo Dimension 3 Degree 9 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 10 &
+nohup ipython tests/test_all_methods.py 3 10 &>method_test_dim3_deg10 &
 echo Dimension 3 Degree 10 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 11 &
+nohup ipython tests/test_all_methods.py 3 11 &>method_test_dim3_deg11 &
 echo Dimension 3 Degree 11 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 3 12 &
+nohup ipython tests/test_all_methods.py 3 12 &>method_test_dim3_deg12 &
 echo Dimension 3 Degree 12 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 4 5 &
+nohup ipython tests/test_all_methods.py 4 5 &>method_test_dim4_deg5 &
 echo Dimension 4 Degree 5 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 5 2 &
+nohup ipython tests/test_all_methods.py 5 2 &>method_test_dim5_deg2 &
 echo Dimension 5 Degree 2 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 5 3 &
+nohup ipython tests/test_all_methods.py 5 3 &>method_test_dim5_deg3 &
 echo Dimension 5 Degree 3 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 6 2 &
+nohup ipython tests/test_all_methods.py 6 2 &>method_test_dim6_deg2 &
 echo Dimension 6 Degree 2 $! >> method_test_pids.txt
 
-nohup ipython tests/test_all_methods.py 7 2 &
+nohup ipython tests/test_all_methods.py 7 2 &>method_test_dim7_deg2 &
 echo Dimension 7 Degree 2 $! >> method_test_pids.txt
