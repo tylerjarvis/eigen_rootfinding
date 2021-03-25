d1ff4389e2855e573dda0a961b99129ec8de3609
Sue Parkinson <suzannastep@gmail.com>
2021-03-24 21:02:14 -0500

    old bug in computing conditioning of eigvals

--- eigen_rootfinding/utils.py
+++ eigen_rootfinding/utils.py
@@ -1384,13 +1384,13 @@ def condeig(A,eig,x,condvec=False):
     """
     n = A.shape[0]
     Q = householder(x)
-    B = ((Q.T.conj())@A@Q)
+    B = ((Q.conj().T)@A@Q)
     R = qr(B[1:,1:]-eig*np.eye(n-1),mode='r')[0]
-    v = solve_triangular(R,-B[0,1:],trans=2)
+    v = solve_triangular(R,-B[0,1:].conj(),trans=2)
     if condvec:
-        return (1+norm(v))**.5,1/(svd(R,compute_uv=False)[-1])
+        return (1+norm(v)**2)**.5,1/(svd(R,compute_uv=False)[-1])
     else:
-        return (1+norm(v))**.5
+        return (1+norm(v)**2)**.5
 
 def condeigs(A,w,v,condvec=False):
     """Estimates the condition numbers of the eigenvalues of A. Optionally
